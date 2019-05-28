7ebbabaf53d46b7a5b5c22389e3e65a20e298c75
Utkarsh <utkarshmandade@gmail.com>
2019-05-28 15:41:05 +0530

    first commit

---
+++ src/main/java/com/globant/springbootdocker/ProductRestController.java
@@ -0,0 +1,30 @@
+
+
+package com.globant.springbootdocker;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductRestController {
+	
+	List<Product> products = new ArrayList<Product>();
+	public ProductRestController() {
+	
+		products.add(new Product(1, "pen", 10));
+		products.add(new Product(2, "book", 50));
+		products.add(new Product(3, "pencil", 5));
+		
+		
+	}
+	
+	@GetMapping("/products")
+	public List<Product> getproducts(){
+		System.out.println("HIT");
+		return products;
+	}
+
+}
